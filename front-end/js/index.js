0c4ba0ea4c9d187cf859e87137de68bc0baa046f
rahmani-sofiane <74100828+rahmani-sofiane@users.noreply.github.com>
2021-02-23 19:48:37 +0100

    add js

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,28 +1,26 @@
 "use strict";
-// Génération de l'API
-
-getMain();
 
 async function getMain() {
-  const articles = await getArticles();
-  displayArticles(articles);
+  const products = await getProducts();
+  displayProducts(products);
 }
 
-function getArticles() {
+function getProducts() {
   return fetch("http://localhost:3000/api/teddies")
     .then(function (httpBodyResponse) {
       return httpBodyResponse.json();
     })
-    .then(function (articles) {
-      console.log(articles);
-      return articles;
+    .then(function (products) {
+      return products;
     });
 }
 
-function displayArticles() {
-  document.getElementById("main");
+function displayProducts(products) {
+  document.getElementsByClassName("productsImage").src = products.imageUrl;
+  document.getElementsByClassName("productsName").textContent = products.name;
+  document.getElementsByClassName("productsDescription").textContent =
+    products.description;
+  document.getElementsByClassName("productsPrice").textContent = products.price;
+  console.log(products);
 }
-
-const displayCaptionElement = function () {
-  document.querySelector(".name-product").textContent = ;
-};
+getMain();
