4ca7cf8be2a00cfdb4694bcd07aef2ea1db00c90
rahmani-sofiane <74100828+rahmani-sofiane@users.noreply.github.com>
2021-02-23 18:00:49 +0100

    add html and js

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+// Génération de l'API
+
+getMain();
+
+async function getMain() {
+  const articles = await getArticles();
+  displayArticles(articles);
+}
+
+function getArticles() {
+  return fetch("http://localhost:3000/api/teddies")
+    .then(function (httpBodyResponse) {
+      return httpBodyResponse.json();
+    })
+    .then(function (articles) {
+      console.log(articles);
+      return articles;
+    });
+}
+
+function displayArticles() {
+  document.getElementById("main");
+}
+
+const displayCaptionElement = function () {
+  document.querySelector(".name-product").textContent = ;
+};
